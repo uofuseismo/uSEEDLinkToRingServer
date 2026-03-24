d185c19db60ed41cd4457f915d4c2b0c2b935ee2
Ben Baker <bakerb845@gmail.com>
2026-03-24 11:00:23 -0600

    seeing if i can get conan to go

--- README.md
+++ README.md
@@ -14,3 +14,31 @@ During the import process, metrics are tabulated on a per-stream (Network, Stati
   6. The standard deviation of the counts in a sampling window (e.g., 6 minutes) per stream
 
 Additionally, to faciliate UUSS's transition to MiniSEED3, packets can be forwarded as MiniSEED2 or MiniSEED3.  The conversion is performed by [libmseed](https://github.com/EarthScope/libmseed).
+
+# Conan
+
+Create a profile Linux-x86_64-clang-21
+
+    [buildenv]
+    CC=/usr/bin/clang-21
+    CXX=/usr/bin/clang++-21
+
+    [settings]
+    arch=x86_64
+    build_type=Release
+    compiler=clang
+    compiler.cppstd=20
+    compiler.libcxx=libstdc++11
+    compiler.version=21
+    os=Linux
+
+    [conf]
+    tools.cmake.cmaketoolchain:generator=Ninja
+
+build-missing downloads and installs missing packages
+Release versions of packages (could set to Debug or other cmake build types)
+
+    conan install . --build=missing -s build_type=Release -pr:a=Linux-x86_64-clang-21 --output-folder ./conanBuild
+    cmake --preset conan-release -DUSE_TBB=ON -DWITH_CONAN=ON
+    cmake --build --preset conan-release
+    ctest --preset conan-release
---
+++ conanfile.py
@@ -0,0 +1,64 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
+
+class uSEEDLinkToRingServerConan(ConanFile):
+   name = "uSEEDLinkToRingServer"
+   #version = "0.4.0"
+   license = "MIT"
+   description = "Propagates files from a SEEDLink client to a RingServer in the same pod in the UUSS Kubernetes environment."
+   url = "https://github.com/uofuseismo/uSEEDLinkToRingServer"
+   topics = ("uSEEDLinkToRingServer")
+   settings = "os", "compiler", "build_type", "arch"
+   options = {"use_tbb": [True, False], "build_tests" : [True, False], "with_conan" : [True, False]}
+   default_options = {"hwloc/*:shared": "True",
+                      "opentelemetry-cpp/*:with_otlp_http": "True",
+                      "opentelemetry-cpp/*:with_otlp_grpc": "True",
+                      "opentelemetry-cpp/*:with_abi_v2" : "True",
+                      "spdlog/*:header_only" : "True",
+                      "use_tbb": "True",
+                      "build_tests" : "True",
+                      "with_conan" : "True",}
+   export_sources = "CMakeLists.txt", "LICENSE", "README.md", "cmake/*", "src/*", "testing/*"
+   generators = "CMakeDeps", "CMakeToolchain"
+
+   def requirements(self):
+       # dependencies
+       self.requires("opentelemetry-cpp/1.24.0")
+       self.requires("boost/1.89.0")
+       self.requires("spdlog/1.17.0")
+       self.requires("onetbb/2022.3.0")
+
+   def build_requirements(self):
+       # test dependncies and build tools
+       self.test_requires("catch2/3.13.0")
+
+   def layout(self):
+       # defines the project layout
+       cmake_layout(self)
+
+   def build(self):
+       # invokes the build system
+       cmake = CMake(self)
+       cmake.configure()
+       cmake.build()
+       #if can_run(self):
+       #   # run tests particularly CTest 
+       #   cmake.test()
+
+   def test(self):
+       if can_run(self):
+          cmake.test()
+
+   #def generate(self):
+   #    tc = CMakeToolchain(self)
+   #    tc.generate()
+
+   def package(self):
+       # copies files from the build to package folder
+       cmake = CMake(self)
+       cmake.install()
+
+   def package_info(self):
+       self.cpp_info.libs = ["uSEEDLinkToRingServer"]
+
